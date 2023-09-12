d6b3792fd9be82b9ece90004957e8b2501bdc7d4
dumbus <dumbusx@gmail.com>
2023-09-12 22:07:53 +0300

    UI lib, controlled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './styles/App.css';
 
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,17 +14,33 @@ function App() {
     {id: 4, title:'JavaScript 4', description: 'JavaScript - Язык программирования'},
     {id: 5, title:'JavaScript 5', description: 'JavaScript - Язык программирования'}
   ]);
+  const [title, setTitle] = useState('');
+  
+  // const descriptionInputRef = useRef();
 
-  const [posts2, setPosts2] = useState([
-    {id: 1, title:'Python', description: 'Python - Язык программирования'},
-    {id: 2, title:'Python 2', description: 'Python - Язык программирования'},
-    {id: 3, title:'Python 3', description: 'Python - Язык программирования'}
-  ]);
+  const addNewPost = (e) => {
+    // e.preventDefault();
+  };
 
   return (
     <div className='App'>
+      <form>
+        {/* Управляемый компонент */}
+        <MyInput 
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type='text' 
+          placeholder='Название поста' 
+        />
+        {/* Неуправляемый элемент
+        <MyInput 
+          ref={descriptionInputRef}
+          type='text' 
+          placeholder='Описание поста' 
+        /> */}
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title='Посты про JavaScript' />
-      <PostList posts={posts2} title='Посты про Python' />
     </div>
   );
 }
