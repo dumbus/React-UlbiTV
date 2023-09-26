74f0ce6d2a1243de7ad02e9d64fd794e4cf85c9b
dumbus <dumbusx@gmail.com>
2023-09-26 21:15:56 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -8,27 +8,36 @@ import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import MyLoader from './components/UI/loader/MyLoader';
+import Pagination from './components/UI/pagination/Pagination';
 
 import PostService from './API/PostService';
 
 import { usePosts } from './hooks/usePosts';
 import { useFetching } from './hooks/useFetching';
 
+import { getPagesCount } from './utils/pages';
+
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
 
   const [fetchPosts, loading, error] = useFetching(async () => {
-    const posts = await PostService.getAllPosts();
-    setPosts(posts);
+    const response = await PostService.getAllPosts(limit, page);
+    setPosts(response.data);
+
+    const totalCount  = response.headers['x-total-count'];
+    setTotalPages(getPagesCount(totalCount, limit));
   });
 
   const sortedAndFilteredPosts = usePosts(posts, filter.sort, filter.query);
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [page]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -37,6 +46,10 @@ function App() {
 
   const deletePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id));
+  };
+
+  const changePage = (page) => {
+    setPage(page);
   };
 
   return (
@@ -57,6 +70,11 @@ function App() {
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><MyLoader /></div>
         : <PostList posts={sortedAndFilteredPosts} title='Посты про JavaScript' deletePost={deletePost} />
       }
+      <Pagination 
+        page={page} 
+        changePage={changePage} 
+        totalPages={totalPages}
+      />
     </div>
   );
 }
